e6117c8cfef838de823549e1cca40fd80f599421
Dan Forsberg <dforsber@gmail.com>
2023-06-17 13:30:17 +0300

    Do not lint generated file

--- generateVersionFile.js
+++ generateVersionFile.js
@@ -1,3 +1,3 @@
 import * as fs from "fs";
 const version = JSON.parse(fs.readFileSync("package.json").toString("utf-8")).version;
-fs.writeFileSync("src/VERSION.ts", `export const VERSION="${version}";\n`);
+fs.writeFileSync("src/VERSION.ts", `export const VERSION = "${version}";\n`);
